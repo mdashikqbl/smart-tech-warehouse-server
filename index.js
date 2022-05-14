772a5870b009bdbcc694b6de91b094842fde3eaa
mdashikqbl <mdashikqbl@gmail.com>
2022-05-14 23:39:30 +0600

    lode data on srever

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const cors = require('cors');
-const { get } = require('express/lib/response');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000
@@ -12,21 +11,28 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.48kqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.um2rt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
     try {
+        await client.connect();
+        const inventoryCollection = client.db('smarttech').collection('Inventory');
 
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventorys = await cursor.toArray();
+            res.send(inventorys);
+        })
     }
     finally {
 
     }
-
 }
+run().catch(console.dir);
 
-run().catch(console.dir)
+
+
 
 
 app.get('/', (req, res) => {
